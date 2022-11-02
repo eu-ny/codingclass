ebdbb517134ba87cf0b237105d0ca0ef20cb2f8d
haeun <praise1109@naver.com>
2022-11-02 16:12:56 +0900

    20221102

---
+++ phpclass/connect/connect.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $user = "praise1109";
+    $pw = "leehaeun3727!";
+    $db = "praise1109";
+    $connect = new mysqli($host, $user, $pw, $db);
+    $connect -> set_charset("utf8");
+    if(mysqli_connect_errno()){
+        echo "database connect false";
+    } else {
+        // echo "database connect true";
+    }
+?>
